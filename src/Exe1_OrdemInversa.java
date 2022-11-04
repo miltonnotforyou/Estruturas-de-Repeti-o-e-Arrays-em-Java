09b3f95aaa1f2a60e5cb39b3dd32a95cb44b31ae
Miltonnnotforyou <67373586+miltonnotforyou@users.noreply.github.com>
2022-11-04 11:54:11 -0300

    Exercicio 1 Arrays

---
+++ src/Exe1_OrdemInversa.java
@@ -0,0 +1,25 @@
+/*
+Crie um vetor de 6 números inteiros
+e mostre-os na ordem inversa.
+*/
+
+public class Exe1_OrdemInversa {
+    public static void main(String[] args) {
+
+        int[] vetor = {-5, -6, 15, 50, 8, 4};
+
+        System.out.println("Vetor: ");
+        int count = 0;
+        while (count < (vetor.length)) {
+            System.out.print(vetor[count] + " ");
+            count++;
+        }
+
+        System.out.println("\nVetor Inverso: ");
+        for (int i = (vetor.length -1); i >= 0; i--) {
+            System.out.print(vetor[i] + " ");
+
+        }
+
+    }
+}
